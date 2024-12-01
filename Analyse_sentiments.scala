781ee180ac544234fa5ee34a2a7a0707374b84d1
Sarahthr <sarah.theret45@gmail.com>
2024-12-01 22:34:21 +0100

    Update Analyse_sentiments.scala

--- Analyse_sentiments.scala
+++ Analyse_sentiments.scala
@@ -4,6 +4,7 @@ import org.apache.spark.ml.feature.Tokenizer
 import org.apache.spark.ml.feature.StopWordsRemover 
 import org.apache.spark.ml.feature.CountVectorizer
 import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.LogisticRegression
 
 val spark = SparkSession.builder.appName("SentimentAnalysis").getOrCreate()
 val tweetsDF = spark.read.option("header", "true").csv("Data3.csv")
