ecd72eba40d343d88c5c6213a550c7703abd12b9
Aimie-grc <aimie.garrec@gmail.com>
2024-12-02 18:17:23 +0100

    Update Analyse_sentiments.scala
    Ajout de la matrice de confusion sur la base de test et séparation de la base en deux

--- Analyse_sentiments.scala
+++ Analyse_sentiments.scala
@@ -4,7 +4,8 @@ import org.apache.spark.ml.feature.{Tokenizer, StopWordsRemover, CountVectorizer
 import org.apache.spark.ml.classification.LogisticRegression
 
 val spark = SparkSession.builder.appName("SentimentAnalysis").getOrCreate()
-val tweetsDF = spark.read.option("header", "true").csv("Data3.csv")
+
+val tweetsDF = spark.read.option("header", "true").csv("H:/Documents/Ing2/S1/Programmation fonctionnelle/Projet/Data3.csv")
 tweetsDF.show(5)
 
 val nettoyerUDF = udf((text: String) => {
@@ -29,19 +30,22 @@ val remover = new StopWordsRemover()
 
 val filteredDF = remover.transform(tokenizedDF)
 
+val updatedDF = tweetsDF.withColumn("sentiment_label",
+  when(col("sentiment") === "positive", 2)
+    .when(col("sentiment") === "negative", 0)
+    .when(col("sentiment") === "neutral", 1)
+    .otherwise(3)
+)
+val Array(trainingTweets, testTweets) = filteredDF.randomSplit(Array(0.8, 0.2), seed = 42)
+trainingTweets.show(5)
+testTweets.show(5)
+
 val hashingTF = new HashingTF().setInputCol("filtered_tokens").setOutputCol("raw_features").setNumFeatures(5000)
-val featurizedDF = hashingTF.transform(filteredDF)
+val featurizedDF = hashingTF.transform(trainingTweets)
 
 val idf = new IDF().setInputCol("raw_features").setOutputCol("features")
 val idfModel = idf.fit(featurizedDF)
 val rescaledDF = idfModel.transform(featurizedDF)
-
-val updatedDF = tweetsDF.withColumn("sentiment_label",
-  when(col("sentiment") === "positive", 1)
-    .when(col("sentiment") === "negative", 0)
-    .when(col("sentiment") === "neutral", 2)
-    .otherwise(3)
-)
 
 val finalDF = rescaledDF.join(updatedDF.select("sentiment_label", "selected_text"), Seq("selected_text"))
 
@@ -57,6 +61,7 @@ val lrModel = lr.fit(finalDataDF)
 val predictions = lrModel.transform(finalDataDF)
 predictions.select("selected_text", "prediction").show(5)
 
+
 val predictionsWithLabels = predictions.select(col("selected_text"), col("sentiment_label"), col("prediction").cast("int"))
 val confusionMatrixDF = predictionsWithLabels.groupBy("sentiment_label", "prediction").count()
 confusionMatrixDF.show()
@@ -68,15 +73,11 @@ val total = predictionsWithLabels.count()
 val accuracy = totalCorrect.toDouble / total * 100
 println(s"Précision du modèle : $accuracy %")
 
-
-
-val newTweetsDF = spark.read.option("header", "true").csv("Data4.csv")
-val cleanedNewTweetsDF = newTweetsDF.withColumn("cleaned_text", nettoyerUDF(col("text")))
-val tokenizedNewTweetsDF = tokenizer.transform(cleanedNewTweetsDF)
-val finalNewTweetsDF = remover.transform(tokenizedNewTweetsDF)
-
-val featurizedNewTweetsDF = hashingTF.transform(finalNewTweetsDF)
+val featurizedNewTweetsDF = hashingTF.transform(testTweets)
+val idfModelNew = idf.fit(featurizedNewTweetsDF)
 val rescaledNewTweetsDF = idfModel.transform(featurizedNewTweetsDF)
+
+val finalDFNew = rescaledNewTweetsDF.join(updatedDF.select("sentiment", "selected_text"), Seq("selected_text"))
 
 val assemblerNew = new VectorAssembler()
   .setInputCols(Array("features"))
@@ -85,9 +86,25 @@ val assemblerNew = new VectorAssembler()
 val finalNewData = assemblerNew.transform(rescaledNewTweetsDF)
 val predictionsNew = lrModel.transform(finalNewData)
 
-predictionsNew.select("text", "cleaned_text", "prediction","sentiment").show(20)
+predictionsNew.select("selected_text", "cleaned_text", "prediction","sentiment").show(20)
 
+val sentiment_labelAdded = predictionsNew.withColumn("sentiment_label",
+  when(col("sentiment") === "positive", 2)
+    .when(col("sentiment") === "negative", 0)
+    .when(col("sentiment") === "neutral", 1)
+    .otherwise(3)
+)
 
+val predictionsWithLabelsNew = sentiment_labelAdded.select(col("text"), col("sentiment_label"), col("prediction").cast("int"))
+val confusionMatrixDFNew = predictionsWithLabelsNew.groupBy("sentiment_label", "prediction").count()
+confusionMatrixDFNew.show()
+val pivotDFNew = confusionMatrixDFNew.groupBy("sentiment_label").pivot("prediction").sum("count").na.fill(0)
+val orderedPivotDFNew = pivotDFNew.orderBy(expr("CASE WHEN sentiment_label= 0 THEN 0 WHEN sentiment_label = 1 THEN 1 ELSE 2 END"))
+orderedPivotDFNew.show()
+val totalCorrectNew = predictionsWithLabelsNew.filter("sentiment_label = prediction").count()
+val totalNew = predictionsWithLabelsNew.count()
+val accuracyNew = totalCorrectNew.toDouble / totalNew * 100
+println(s"Précision du modèle : $accuracyNew %")
 
 val totalTweets = predictionsNew.count()
 val positiveTweets = predictionsNew.filter(col("prediction") === 1).count()
